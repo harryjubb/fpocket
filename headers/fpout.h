8dfc936fd5a060ff2217e7a0956c004c4cd35d1f
Harry Jubb <hj4@sanger.ac.uk>
2018-04-28 16:10:44 +0100

    Resolves Discngine/fpocket#5

--- headers/fpout.h
+++ headers/fpout.h
@@ -52,6 +52,7 @@
 /* -----------------------------PROTOTYPES------------------------------------*/
 
 void write_out_fpocket(c_lst_pockets *pockets, s_pdb *pdb, char *pdbname) ;
+void write_out_fpocket_info_file(c_lst_pockets *pockets, char *output_file_name);
 
 void write_descriptors_DB(c_lst_pockets *pockets, FILE *f);
 void write_out_fpocket_DB(c_lst_pockets *pockets, s_pdb *pdb, char *input_name);
